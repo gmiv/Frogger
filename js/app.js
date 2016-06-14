d29970ab57c45aa1046d87bff3ec2e904eb7cee0
Geronimo Moralez <Gmora7@gmail.com>
2016-06-13 22:59:15 -0700

    Cleanup, added Main

--- js/app.js
+++ js/app.js
@@ -1,35 +1,63 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// ENEMY
+var Enemy = Object.create(Transform);
+Enemy.prototype.constructor = Enemy;
+
+var Enemy = function () {
+    this.position = [Rand(-100,-10), Rand(50,180)];
+    this.speed = Rand(50,180);
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    
+    if(dt !== 0){
+        this.position = [this.position[0] + this.speed * dt, this.position[1]];
+        //console.log(this.position); 
+        };
+    
+    if( this.position[0] > 550){
+        this.reset();
+        };
 };
 
-// Draw the enemy on the screen, required method for game
+Enemy.prototype.reset = function() {
+    this.position = [Rand(-100,-10), Rand(50,180)];
+}
+
 Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.position[0], this.position[1]);
+};
+// END ENEMY
+
+
+// PLAYER
+
+var Player = function() {
+
+    this.position = [];
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.handleInput = function(key){
+    
+};
+
+
+Player.prototype.update = function(dt) {
+
+};
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+// END PLAYER
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// UTILITIES
+var Rand = function (min, max) {
+    return Math.floor((Math.random() * max) + min);
+};
+// END UTILITIES
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +72,20 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// MAIN
+var player
+var allEnemies
+var Main = function(){
+    
+    player = new Player();
+
+    allEnemies = [];
+    for (var i = 0; i < 2; i++) {
+        allEnemies.push(new Enemy());
+    };  
+};
+// END MAIN
+
+Main();
+
